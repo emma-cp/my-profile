2609d8118c74eb69b994f94e838e1835c17d0669
chanakarn punyachai <Chanakarn@chanakarns-MacBook-Air.local>
2021-08-07 21:42:14 +1000

    First Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// If flavor equals "chocolate", //
+//vessel equals "cone" and toppings equals //
+// "sprinkles", then "I'd like two scoops of chocolate ice cream in a cone with sprinkles.
+//" should be printed to the console. //
+
+
+
+var flavor ='vanilla';
+var vessel ='bowl';
+var toppings ='cookies';
+
+if (flavor === 'chocolate') {
+    console.log("I'd like two scoops of chocolate")
+}
+else if (vessel === 'cone') {
+    console.log('ice cream in a cone')
+}
+else if (topping === sprinkles) {
+    console.log('with sprinkles')
+}
+else {
+    console.log("I will on diet")
+}
